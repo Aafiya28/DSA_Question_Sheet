ef5e50d727bf40927236d2228eac5508c6f2e9d9
Aafiya Afroz <aafiyaafroz97.gmail.com>
2026-01-14 12:37:59 +0530

    DSA Question

---
+++ Arrays/Largest_Element.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Largest_Element {
+
+    static int largest(int[] arr){
+
+        int n = arr.length;
+
+        int largest = arr[0];
+
+        for(int i=1; i<n; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+
+        return largest;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 6, 23, 7, 12, 9, 16};
+
+        int largest_value = largest(arr);
+
+        System.out.println("Largest value of this array: " + largest_value);
+    }
+}
