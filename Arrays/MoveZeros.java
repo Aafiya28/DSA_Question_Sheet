5db6c739ffd21b60760e792caa98fb8877fc97a2
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-01-19 13:51:29 +0530

    Moves Zeros form Array

---
+++ Arrays/MoveZeros.java
@@ -0,0 +1,16 @@
+public class MoveZeros {
+
+    static void moveZero(int[] arr){
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {34, 67, 36, 98, 56, 72};
+        int n =arr.length;
+
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
