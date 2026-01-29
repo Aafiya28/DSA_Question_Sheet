1e1068b41cc61be52cd0da9d544747f85b27de4a
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-01-29 13:08:17 +0530

    Frequency of "X" element in array

---
+++ Arrays/FrequencyOfX.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class FrequencyOfX {
+
+    static int findFreq(int[] arr, int X){
+
+        int n=arr.length;
+        int freq = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] == X){
+                freq++;
+            }
+        }
+
+        return freq;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of element in array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.print("Enter element in array: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to find its frequency: ");
+        int X = sc.nextInt();
+
+        int ans = findFreq(arr, X);
+        System.out.println(ans);
+    }
+}
