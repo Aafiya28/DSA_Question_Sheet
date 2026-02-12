a7dac16b02668fdba3a34d556e99bc584af82b10
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-12 23:58:37 +0530

    Two Pointer

---
+++ src/Searching/IsPair.java
@@ -0,0 +1,13 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class IsPair {
+
+    static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter");
+    }
+}
