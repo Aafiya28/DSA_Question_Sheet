a97d47957845363e43e87d26345aad78641adea3
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-12 23:53:15 +0530

    Get Peak Element

--- src/Searching/PeakElement.java
+++ src/Searching/PeakElement.java
@@ -11,7 +11,7 @@ public class PeakElement {
 
         ArrayList<Integer> peakValue = new ArrayList<>();
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<=n; i++){
 
             if(arr[i] >= arr[i+1]){
                 peakValue.add(arr[i]);
@@ -25,22 +25,49 @@ public class PeakElement {
         return peakValue;
     }
 
+    static int getPeak(int[] arr){
+
+        int n = arr.length;
+
+        int low = 0, high = n - 1;
+
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if((mid == 0 || arr[mid - 1] <= arr[mid]) &&
+                    (mid == n - 1 || arr[mid + 1] <= arr[mid]))
+                return mid;
+            if(mid > 0 && arr[mid - 1] >= arr[mid])
+                high = mid -1;
+            else
+                low = mid + 1;
+        }
+
+        return -1;
+    }
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter no. of element in array: ");
-        int n = sc.nextInt();
+//        System.out.print("Enter no. of element in array: ");
+//        int n = sc.nextInt();
+//
+//        int[] arr = new int[n];
+//
+//        System.out.println("Enter elements in array: ");
+//        for(int i=0; i<n; i++){
+//            arr[i] = sc.nextInt();
+//        }
 
-        int[] arr = new int[n];
+//        ArrayList<Integer> result = peakEle(arr);
 
-        System.out.println("Enter elements in array: ");
-        for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
-        }
+        int arr[] = {5, 20, 40, 30, 20, 50, 60}, n = 7;
 
-        ArrayList<Integer> result = peakEle(arr);
 
-        System.out.println("Peak Element of Array: " + result);
+        int peakEle = getPeak(arr);
+
+//        System.out.println("Peak Element of Array: " + result);
+        System.out.println("Peak Element of Array: " + peakEle);
     }
 }
