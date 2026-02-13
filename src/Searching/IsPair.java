73a5f3ebab1aa8171b3cb7b1797c11c513e72007
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-13 23:52:17 +0530

    Is pair present

--- src/Searching/IsPair.java
+++ src/Searching/IsPair.java
@@ -8,6 +8,16 @@ public class IsPair {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter");
+        System.out.println("Enter no. of elements in array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements in array: ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+
     }
 }
