c4437ffc878f28cb39fb58500533ce2c8068447a
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-16 23:57:58 +0530

    Majority Element

--- src/Searching/MajorityEle.java
+++ src/Searching/MajorityEle.java
@@ -6,8 +6,20 @@ public class MajorityEle {
 
     static int majorityEle(int[] arr){
 
-        int n = 0;
+        int n = arr.length;
 
+        for(int i=0; i<n; i++){
+            int count = 1;
+            for(int j=i+1; j<n; j++){
+                if (arr[i] == arr[j]){
+                    count++;
+                }
+            }
+
+            if(count > n/2){
+                return i;
+            }
+        }
 
         return -1;
     }
