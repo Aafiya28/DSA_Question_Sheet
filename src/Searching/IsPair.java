540e0c21cd6701604ea6e1d29f3ea7e1ea41eb95
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-17 23:45:17 +0530

    Sheet Updated

--- src/Searching/IsPair.java
+++ src/Searching/IsPair.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class IsPair {
 
+    static int pair(int[] arr){
+
+        return -1;
+    }
+
     static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
