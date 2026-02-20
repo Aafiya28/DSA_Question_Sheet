b6e1793d18ffc1d29001c616506058f077ec582f
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-20 23:58:47 +0530

    Repeat Element

--- src/Searching/RepeatEle.java
+++ src/Searching/RepeatEle.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class RepeatEle {
 
+    static int repeatElement(int[] arr){
+
+        int n = arr.length;
+
+        int count = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+
+
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
