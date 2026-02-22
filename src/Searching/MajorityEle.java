80c0ae467cca42b384b52ab06d851e0483a1eacd
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-22 23:55:00 +0530

    Pair of Element

--- src/Searching/MajorityEle.java
+++ src/Searching/MajorityEle.java
@@ -17,7 +17,7 @@ public class MajorityEle {
             }
 
             if(count > n/2){
-                return arr[i];
+                return i;
             }
         }
 
@@ -33,6 +33,7 @@ public class MajorityEle {
 
         int[] arr = new int[n];
 
+        System.out.print("Enter elements in array: ");
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
