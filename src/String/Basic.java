9cd03a898cfbb30deb51c501a2688e82865b8aca
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-24 23:53:05 +0530

    Basic of String

---
+++ src/String/Basic.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Basic {
+
+    public static void main(String[] args){
+
+        String str = "geeks";
+
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.contains("s"));
+        System.out.println(str.split("ge", 2));
+    }
+}
