55c6d8d3d91a65d1bbf5756ce60c7bb8706f6cca
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-02-28 12:14:29 +0530

    Palindrome

--- src/String/Palindrome.java
+++ src/String/Palindrome.java
@@ -6,9 +6,19 @@ public class Palindrome {
 
     static boolean isPalindrome(String str){
 
+        int n = str.length();
+        int left = 0;
+        int right = n-1;
 
+        while(left < right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
 
-        return false;
+        return true;
     }
 
     public static void main(String[] args){
