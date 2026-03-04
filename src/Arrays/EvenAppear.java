cd4b5b6757e166b91bb5b2524825c85dc616267f
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-03-04 11:31:05 +0530

    Sheet Updated

---
+++ src/Arrays/EvenAppear.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class EvenAppear {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!mp.containsKey(arr[i])) {
+                mp.put(arr[i], 1);
+            } else {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            }
+        }
+
+        for (HashMap.Entry<Integer, Integer> entry : mp.entrySet()) {
+            if (entry.getValue() % 2 == 0) {
+                System.out.print(entry.getKey() + " ");
+            }
+        }
+    }
+}
--- src/Arrays/RemoveDublicate.java
+++ src/Arrays/RemoveDublicate.java
@@ -1,6 +1,26 @@
 package Arrays;
 
+import java.util.ArrayList;
+
 public class RemoveDublicate {
+
+    static ArrayList<Integer> removeDuplicates(int[] arr) {
+        // code here
+
+        ArrayList<Integer> result = new ArrayList<>();
+        int n = arr.length;
+
+        if(n == 0) return result;
+        result.add(arr[0]);
+
+        for(int i=1; i<n; i++){
+            if(arr[i-1] != arr[i]){
+                result.add(arr[i]);
+            }
+        }
+
+        return result;
+    }
 
     static int remDups(int arr[], int n)
     {
@@ -32,6 +52,11 @@ public class RemoveDublicate {
         for(int i = 0; i < n; i++) {
             System.out.print(arr[i]+" ");
         }
+        System.out.println();
 
+        int[] arr2 = {2,2,2,2};
+
+        ArrayList<Integer> result  = removeDuplicates(arr);
+        System.out.println(result);
     }
 }
