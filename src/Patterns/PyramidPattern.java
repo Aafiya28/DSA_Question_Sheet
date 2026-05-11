f3bd5e7739b632d514430b3869af22fa7d3f3dfe
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-05-11 12:53:21 +0530

    Patterns Problems

--- src/Patterns/PyramidPattern.java
+++ src/Patterns/PyramidPattern.java
@@ -1,28 +1,24 @@
 package Patterns;
 
-import java.util.Scanner;
-
-public class RhombusPattern {
+public class PyramidPattern {
+    //Solid Pyramid Pattern
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-//        int row = sc.nextInt();
-//        int col = sc.nextInt();
         int row = 5;
         int col = 9;
 
-        for(int i=0; i<=row; i++){
+        for(int i=1; i<=row; i++){
+
+            //for spacing
             for(int j=row-i; j>0; j--){
                 System.out.print("  ");
             }
 
-            for(int k=0; k<=row; k++){
+            for(int k=1; k<= 2*i-1; k++){
                 System.out.print("* ");
             }
 
             System.out.println();
-
         }
     }
 }
