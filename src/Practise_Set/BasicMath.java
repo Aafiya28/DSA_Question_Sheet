991da4ca546c98aea654610341c536aa0e5626da
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-06-05 11:06:39 +0530

    Mathematical Problems & String Based DSA Problems

---
+++ src/Practise_Set/BasicMath.java
@@ -0,0 +1,126 @@
+package Practise_Set;
+
+public class BasicMath {
+
+    static void printNum(int num){
+
+        while (num != 0){
+            int digit = num%10;
+            System.out.print(digit + ", ");
+            num = num/10;
+        }
+        System.out.println();
+    }
+
+    static int countDigit(int num){
+
+        int count = 0;
+        while (num != 0){
+            int digit = num%10;
+            count++;
+            num = num/10;
+        }
+        return count;
+    }
+
+    static int sumOfDigit(int num){
+
+        int sum = 0;
+
+        while (num != 0){
+            int digit = num%10;
+            sum += digit;
+            num = num/10;
+        }
+        return sum;
+    }
+
+    static void reverseDigits(int num){
+
+        int reverse = 0;
+
+        while (num != 0){
+            int digit = num%10;
+            reverse = reverse * 10 + digit;
+            num = num/10;
+        }
+        System.out.println(reverse);;
+    }
+
+    static boolean palindromeCheck(int num){
+
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0){
+            int digit = num%10;
+            reverse = reverse * 10 + digit;
+            num = num/10;
+        }
+
+        if(reverse == original){
+            return true;
+        }
+        System.out.println(reverse  + " & " + original);
+        return false;
+    }
+
+    static boolean isPrime(int num){
+
+        for(int i=2; i*i<=num; i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int getGCD(int a, int b){
+
+        while (b != 0){
+
+            int oldValueOfB = b;
+            b = a%b;
+            a = oldValueOfB;
+        }
+        int ans = a;
+
+        return ans;
+    }
+
+    static long getLCM(int a, int b){
+
+        int originalA = a;
+        int originalB = b;
+
+        while (b != 0){
+            int oldValueOfB = b;
+            b = a%b;
+            a = oldValueOfB;
+        }
+
+        return ((long) originalA * originalB) / a ;
+
+    }
+
+    public static void main(String[] args) {
+
+        int num = 1498;
+
+        printNum(num);
+        System.out.println("Total digits in " + num + " are: " + countDigit(num));
+
+        System.out.println("Sum of all digits of " + num  + " is: " + sumOfDigit(num));
+
+        System.out.print("Reverse of " + num + " is: ");
+        reverseDigits(num);
+
+        System.out.println(num + " is palindrome: " + palindromeCheck(5445));
+
+        System.out.println("Check is" + 12 + " is prime number: " + isPrime(11));
+
+        System.out.println("GCD/HCF of " + 24 + " & " + 12 + " is: " + getGCD(24, 12));
+
+        System.out.println("LCM of " + 24 + " & " + 12 + " is: " + getLCM(24, 12));
+    }
+}
--- src/String/Practise.java
+++ src/String/Practise.java
@@ -2,6 +2,7 @@ package String;
 
 public class Practise {
 
+    //2. Count length of String without using length() method:
     static int getLengthOfStr(String str){
 
         char[] chArr = str.toCharArray();
@@ -12,6 +13,7 @@ public class Practise {
         return count;
     }
 
+    //3. Count Vowels in String:
     static int getCountVowels(String str){
 
         int len = str.length();
@@ -26,6 +28,7 @@ public class Practise {
         return count;
     }
 
+    //4. Reverse a String:
     static void gerReverseOfString(String str){
 
         int len = str.length();
@@ -40,6 +43,7 @@ public class Practise {
         System.out.println(reverse);
     }
 
+    //5. Check String is Palindrome or not:
     static boolean getPalindrome(String str){
 
         int len = str.length();
@@ -57,6 +61,125 @@ public class Practise {
             }
             left++;
             right--;
+        }
+        return true;
+    }
+
+    //6A. Converting string to uppercase without using method:
+    static void getUppercaseOfString(String str){
+
+        int len = str.length();
+
+        String newStr = "";
+
+        for(int i=0; i<len; i++){
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char) (ch - ('a' - 'A'));
+            }
+            newStr += ch;
+        }
+
+        System.out.println(newStr);
+    }
+
+    //6B. Converting string into lowercase without using method:
+    static void getLowercaseOfString(String str){
+
+        int len = str.length();
+
+        String newStr = "";
+
+        for(int i=0; i<len; i++){
+
+            char ch = str.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                ch = (char) (ch - ('A' - 'a'));
+            }
+            newStr += ch;
+        }
+
+        System.out.println(newStr);
+    }
+
+    //7. Count Consonant in a String:
+    static int getCountOfConsonant(String str){
+
+        int len = str.length();
+        int count = 0;
+
+        for(int i=0; i<len; i++){
+            char ch = Character.toLowerCase(str.charAt(i));
+            if(ch >= 'a' && ch <= 'z' && ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //8. Finding Frequency of a character in a String:
+    static void freqOfString(String str){
+
+        int len = str.length();
+        int count = 1;
+
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                if(str.charAt(i) == str.charAt(j)){
+                    count++;
+                }else {
+                    count = 1;
+                }
+            }
+            System.out.println(str.charAt(i) + ": " + count);
+        }
+    }
+
+    //9. Remove Spaces from String:
+    static void getRemoveSpaces(String str){
+
+        int len = str.length();
+
+        String newStr = "";
+
+        for(int i=0; i<len; i++){
+            char ch = str.charAt(i);
+            if(ch != ' '){
+                newStr += ch;
+            }
+        }
+        System.out.println(newStr);
+    }
+
+    //10. Count word in a Sentence:
+    static int getCountOfWord(String str){
+
+        int len = str.length();
+        int count = 1;
+
+        for(int i=0; i<len; i++){
+            char ch = str.charAt(i);
+            if(ch == ' '){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //11. Check if string contains only digits:
+    static boolean getContainDigit(String str) {
+
+        int len = str.length();
+
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
         }
         return true;
     }
@@ -85,5 +208,24 @@ public class Practise {
 
         //5. Check String is Palindrome or not:
         System.out.println(getPalindrome("noon"));
+
+        //6. Converting string to uppercase without using method:
+        getUppercaseOfString("uppercase string");
+        getLowercaseOfString("LOWERCASE STRING");
+
+        //7. Count Consonant in a String:
+        System.out.println("Total count of consonant present in this string are: " + getCountOfConsonant(str));
+
+        //8. Finding Frequency of a character in a String:
+        freqOfString("java");
+
+        //9. Remove Spaces from String:
+        getRemoveSpaces("Practise in");
+
+        //10. Count word in a Sentence:
+        System.out.println(getCountOfWord(str));
+
+        //11. Check if string contains only digits:
+        System.out.println("Is string contains only digits: " + getContainDigit("12"));
     }
 }
