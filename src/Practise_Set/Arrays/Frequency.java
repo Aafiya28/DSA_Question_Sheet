1b9c852007809873e2852394cf2360f57a594c3c
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-06-19 20:30:19 +0530

    Maximum Frequency Count

---
+++ src/Practise_Set/Arrays/Frequency.java
@@ -0,0 +1,24 @@
+package Practise_Set.Arrays;
+
+public class Frequency {
+
+    static int highestFreq(int[] arr){
+
+        int n = arr.length;
+
+        int count = 0;
+
+        return count;
+    }
+
+    static int lowestFreq(int[] arr){
+
+        int n = arr.length;
+
+        int count = 0;
+
+        return count;
+    }
+
+
+}
