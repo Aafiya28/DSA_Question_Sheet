c7ea1d755501aee57528c8eb1d555d3a6b61fda4
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-07-28 13:38:17 +0530

    Reverse an Array

--- src/Arrays/Reverse.java
+++ src/Arrays/Reverse.java
@@ -28,26 +28,17 @@ public class Reverse {
 
         public static void main(String[] args) {
             int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            int[] var2 = arr;
-            int var3 = arr.length;
 
-            int var4;
-            int num;
-            for(var4 = 0; var4 < var3; ++var4) {
-                num = var2[var4];
-                System.out.print("" + num + " ");
+            for(int n : arr){
+                System.out.print(n + ", ");
             }
 
             System.out.println();
             reverseArr(arr);
-            var2 = arr;
-            var3 = arr.length;
 
-            for(var4 = 0; var4 < var3; ++var4) {
-                num = var2[var4];
-                System.out.print("" + num + " ");
+            for(int a : arr){
+                System.out.print(a + ", ");
             }
-
         }
 
 }
