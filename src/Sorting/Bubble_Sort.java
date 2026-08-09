758667d5ae538aeaec51558e35bb1f8057615dc4
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-08-09 15:50:01 +0530

    Recursive Bubble Sort

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -21,12 +21,26 @@ public class Bubble_Sort {
             if(swapped == false)
                 break;
         }
+    }
 
+    static void recursiveBubbleSort(int[] arr, int low, int high){
+
+        if(high == 0) return;
+
+        if (low < high){
+
+            if(arr[low] > arr[low+1]){
+                int temp = arr[low];
+                arr[low] = arr[low+1];
+                arr[low+1] = temp;
+            }
+            recursiveBubbleSort(arr, low+1, high);
+        }else {
+            recursiveBubbleSort(arr, 0, high-1);
+        }
     }
 
     public static void main(String[] args) {
-
-        int[] arr = {64, 25, 12, 22, 11, 5};
 
         int[] nums = {13, 46, 24, 52, 20, 9};
 
@@ -36,10 +50,27 @@ public class Bubble_Sort {
         }
         System.out.println();
 
+        //Bubble Sort Method
         bubbleSort(nums);
-
         System.out.print("After using Bubble Sort Algorithm Sorted array is: ");
         for(int num : nums){
+            System.out.print(num + ", ");
+        }
+        System.out.println();
+
+        System.out.println();
+
+        int[] arr = {64, 25, 12, 22, 11, 5};
+
+        for(int num : arr){
+            System.out.print(num + ", ");
+        };
+        System.out.println();
+
+        //Recursive Bubble Sort Method
+        recursiveBubbleSort(arr, 0, arr.length-1);
+        System.out.print("Using Recursive Bubble Sort Method to Sort Array: ");
+        for(int num : arr){
             System.out.print(num + ", ");
         }
     }
