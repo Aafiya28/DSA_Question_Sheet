1a23bb3fa5989f94274d566d976d61f6eb95c27d
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-08-09 15:52:17 +0530

    Recursive Bubble Sort

--- src/Sorting/Bubble_Sort.java
+++ src/Sorting/Bubble_Sort.java
@@ -9,7 +9,7 @@ public class Bubble_Sort {
         for(int i=0; i<n-1; i++){
             boolean swapped = false;
 
-            for(int j=0; j<n-i - 1; j++){
+            for(int j=0; j<n-i-1; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
