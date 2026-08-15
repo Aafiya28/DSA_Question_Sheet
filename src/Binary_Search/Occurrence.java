5e687011c689bff77ba87b61e924c84705e313e3
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-08-15 16:44:48 +0530

    First and Last Occurrence

--- src/Binary_Search/Occurrence.java
+++ src/Binary_Search/Occurrence.java
@@ -23,12 +23,65 @@ public class Occurrence {
         return new int[] {first, last};
     }
 
+    //For First&LastOcc Using Binary Search Separately finding first and last occurrence
+    static int firstOccDirect(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                high = mid - 1; // keep looking on the left
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int lastOccDirect(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;
+                low = mid + 1; // keep looking on the right
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int[] firstAndLastOcc(int[] arr, int x){
+
+        int first = firstOccDirect(arr, x);
+        if(first == -1) return new int[]{-1,-1};
+
+        int last = lastOccDirect(arr, x);
+
+        return new int[] {first, last};
+    }
+
     public static void main(String[] args) {
 
         int[] arr = {3,4,6,13,13,13,20,40};
         int target = 13;
 
-        int[] result = firstAndLast(arr, target);
+        int[] result = firstAndLastOcc(arr, target);
 
         for(int num : result){
             System.out.print(num + ", ");
