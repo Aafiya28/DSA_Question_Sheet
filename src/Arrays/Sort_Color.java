cf76b04baa9cfe3549c6d76cb071f83939935046
Aafiya Afroz <142248354+Aafiya28@users.noreply.github.com>
2026-08-30 23:00:55 +0530

    Sort Color

---
+++ src/Arrays/Sort_Color.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+public class Sort_Color {
+
+    static void sortColor(int[] arr){
+
+        int n = arr.length;
+
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while (mid <= high){
+
+            if(arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                mid++;
+                low++;
+            }else if(arr[mid] == 1){
+                mid++;
+            }else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2,0,2,1,1,0};
+
+        sortColor(arr);
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+}
